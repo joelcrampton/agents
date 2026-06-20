45d74b0d9a517f6624c6c3988dbd96196f4fbe36
cramptjoel <cramptjoel@myvuw.ac.nz>
2026-06-21 03:29:13 +1200

    Created a searchWeb tool

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # agents
+
+## Notes
+- GPT models are trained with data up to a certain date
+- If we ask questions about current events beyond that date it has no record of them
+- Using tools, we can provide additional information/guidance
+- For example a searchWeb() tool to allow the model to search the web when it needs external context
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,53 @@
 import 'dotenv/config';
-import { ToolLoopAgent, generateText } from 'ai';
+import { ToolLoopAgent, tool } from 'ai';
 import { google } from '@ai-sdk/google';
+import z from 'zod';
+import { tavily } from '@tavily/core';
+
+const client = tavily({
+  apiKey: process.env.TAVILY_API_KEY!
+});
 
 const gemini = new ToolLoopAgent({
   model: google('gemini-2.5-flash'),
   instructions: 'You are a helpful assistant.',
+  tools: {
+    searchWeb: tool({
+      description: 'Search the web for information',
+      inputSchema: z.object({
+        query: z.string(),
+        limit: z.number().min(1).max(10).default(10)
+      }),
+      execute: async ({ query, limit }) => {
+        const response = await client.search(query, {
+          searchDepth: 'basic',
+          maxResults: limit
+        });
+        console.log(`Executing searchWeb for ${limit} results.`);
+        return response.results.map((result) => ({
+          title: result.title,
+          url: result.url,
+          content: result.content,
+        }));
+      },
+    })
+  },
+  toolChoice: {
+    type: 'tool',
+    toolName: 'searchWeb' // Force the searchWeb tool to be used on every step
+  },
+  prepareStep: ({ steps }) => {
+    const searched = steps.some(step => step.toolCalls?.some(tc => tc.toolName === 'searchWeb'));
+    if (searched) {
+      // Force a text-only response on the next step after the searchWeb tool is used
+      // Overrides the base toolChoice settings
+      return { toolChoice: 'none' }; // Or activeTools: []
+    }
+  },
 });
 
 const result = await gemini.generate({
-  prompt: 'Hello.',
+  prompt: 'What are the top 3 news headlines today?',
 });
 
 console.log(result.text);
